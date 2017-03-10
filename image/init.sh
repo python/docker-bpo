ea0144a726a4792042df3935fa0ab72b326747e9
Maciej Szulik <maszulik@redhat.com>
2017-03-10 23:06:32 +0100

    Fix the wait for postgresql

--- image/init.sh
+++ image/init.sh
@@ -11,11 +11,18 @@ function stop_all() {
 trap stop_all HUP INT QUIT KILL TERM
 
 
+# wait for postgresql to be available
+while true; do
+    curl --max-time 2 --fail --silent http://localhost:5432/
+    if [[ $? -eq 52 ]]; then
+        break
+    fi
+    sleep 1
+done
+
 # initialize the db (pwd: admin) and create users
-sleep 5  # wait for PG to start...
 /home/tracker/bin/roundup-admin -i /opt/tracker/python-dev init admin
 python3 /home/tracker/bin/createusers.py
-
 
 if [ $# -eq 0 ]; then
     /bin/bash
