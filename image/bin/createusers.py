fe891bda18ad9ee2262fa25688883f713412c415
Ezio Melotti <ezio.melotti@gmail.com>
2017-03-10 23:13:23 +0200

    #3: address review comments by adding comments to createusers.py.

--- image/bin/createusers.py
+++ image/bin/createusers.py
@@ -16,12 +16,15 @@ users = ['user', 'developer', 'coordinator']
 roles = []
 
 for user in users:
+    # listing the role is not enough -- all lower-level roles
+    # must be added too (e.g. Coordinator,Developer,User)
     roles.append(user.capitalize())
     r = ','.join(roles)
     ic = 1 if user == 'coordinator' else 0  # only coord is committer
     args = ('create user username={u} address={u}@bugs.python.org '
             'password=pass realname={u} github={u} roles={r} '
             'iscommitter={ic}'.format(u=user, r=r, ic=ic))
+    # developer and coordinator signed the cla
     if user in {'developer', 'coordinator'}:
         args += ' contrib_form=1 contrib_form_date=2017-03-22.00:00:00'
 
